6686cae2416b2e032d17043e237e6cac7fb063d5
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-04 14:18:54 -0700

    Add a database to store the food menu data

---
+++ migrations/food_menu.js
@@ -0,0 +1,42 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  // Create a food_types schema
+  return knex.schema.createTable('food_types', (table) => {
+    table.string('type').primary();
+    table.string('image').notNullable().defaultTo('');
+    table.integer('priority').notNullable().defaultTo(1000);
+  })
+    .then(() => {
+      // Create a food_items schema
+      return knex.schema.createTable('food_items', (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
+        table.string('name').notNullable().unique();
+        table.text('description').notNullable().defaultTo('');
+        table.decimal('price', 10, 2).notNullable().defaultTo(0.0);
+        table.string('image').notNullable().defaultTo('');
+        table.integer('priority').notNullable().defaultTo(1000);
+        table.string('type')
+          .notNullable()
+          .references('type')
+          .inTable('food_types')
+          .onUpdate('CASCADE')
+          .onDelete('CASCADE');
+      });
+    });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  // Delete the food_items schema
+  return knex.schema.dropTableIfExists('food_items')
+    .then(() => {
+      // Delete the food_types schema
+      return knex.schema.dropTableIfExists('food_types');
+    });
+}
