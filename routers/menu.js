e2ee772bd4d6cbf542e0d8a8f9978b504964a81d
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-06 18:15:49 -0700

    Update the GET requests of the menu

--- routers/menu.js
+++ routers/menu.js
@@ -8,40 +8,26 @@ const knex = initKnex(development);
 
 // Get the menu of food types
 router.get('/foodtypes', async (req, res) => {
-    const foodTypes = await knex
-        .select('T.type', 'T.image')
-        .from('food_types as T')
-        .orderBy('T.priority');
+    const foodTypes = await knex('food_types')
+        .select('type', 'image')
+        .orderBy('priority');
     return res.json(foodTypes);
 });
 
 // Get the menu of food items
 router.get('/fooditems', async (req, res) => {
-    // Get the food types from the database
-    const foodTypes = await knex('food_types');
+    const { foodtype } = req.body;
 
-    // Create a menu
-    const menu = {};
+    const foodItems = await knex('food_items')
+        .select('id', 'name', 'description', 'price', 'image')
+        .where('type', foodtype)
+        .orderBy('priority');
 
-    // Create an array for each type
-    foodTypes.forEach((row) => {
-        menu[row.type] = [];
-    })
+    if (foodItems.length === 0) {
+        return res.status(400).send();
+    }
 
-    // Get the food items from the database
-    const foodItems = await knex
-      .select('T.type', 'I.id', 'I.name', 'I.description', 'I.price', 'I.image')
-      .from('food_types as T')
-      .join('food_items as I', 'T.type', 'I.type')
-      .orderBy(['T.priority', 'I.priority']);
-
-    // Insert the food items into its array
-    foodItems.forEach((row) => {
-        const { type, id, name, description, price, image } = row;
-        menu[type].push({ id, name, description, price, image });
-    })
-
-    return res.json(menu);
+    return res.json(foodItems);
 });
 
 export default router;
