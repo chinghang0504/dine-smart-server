4449a26c1a141847a65e7507b6baf9d2d2189722
Ching Hang Lam <chinghang0504@gmail.com>
2024-07-06 20:11:05 -0700

    Update the GET requests in the menu

--- routers/menu.js
+++ routers/menu.js
@@ -7,29 +7,37 @@ const router = express.Router();
 const knex = initKnex(development);
 
 // Get the menu of food types
-router.get('/foodtypes', async (req, res) => {
-    const foodTypes = await knex('food_types')
-        .select('type', 'image')
-        .orderByRaw('priority IS NULL')
-        .orderBy('priority');
+router.get("/foodtypes", async (_req, res) => {
+  try {
+    const foodTypes = await knex("food_types")
+      .select("type", "image")
+      .orderByRaw("priority IS NULL")
+      .orderBy("priority");
     return res.json(foodTypes);
+  } catch {
+    return res.status(500).send();
+  }
 });
 
 // Get the menu of food items
-router.get('/fooditems', async (req, res) => {
-    const { foodtype } = req.body;
+router.get("/fooditems", async (req, res) => {
+  const { foodtype } = req.body;
 
-    const foodItems = await knex('food_items')
-        .select('id', 'name', 'description', 'price', 'image')
-        .where('type', foodtype)
-        .orderByRaw('priority IS NULL')
-        .orderBy('priority');
+  try {
+    const foodItems = await knex("food_items")
+      .select("id", "name", "description", "price", "image")
+      .where("type", foodtype)
+      .orderByRaw("priority IS NULL")
+      .orderBy("priority");
 
     if (foodItems.length === 0) {
-        return res.status(400).send();
+      return res.status(400).send();
     }
 
     return res.json(foodItems);
+  } catch {
+    return res.status(500).send();
+  }
 });
 
 export default router;
